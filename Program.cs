686b7ae8733b0763f32efc9ca39024756d12e727
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-08-01 00:01:29 +0200

    Symbolic start of my C# & .NET learning

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static string Function(string name)
+{
+    string hello = "Hello ";
+    string text = hello + name.ToString();
+    return text;
+}
+
+static void StartProgram()
+{
+    Console.WriteLine("Hello, what's your name?");
+    string name = Console.ReadLine();
+    if (!String.IsNullOrEmpty(name))
+    {
+        Console.WriteLine(Function(name));
+    }
+    else
+    {
+        Console.WriteLine("Please write your name!");
+        StartProgram();
+    }
+}
+StartProgram();
